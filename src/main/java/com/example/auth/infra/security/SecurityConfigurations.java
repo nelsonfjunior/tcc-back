cff9e26a6e8f6d7af3f02acb2e6006a85a7cfd9d
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-03-28 17:51:49 -0300

    feat: cors config

--- src/main/java/com/example/auth/infra/security/SecurityConfigurations.java
+++ src/main/java/com/example/auth/infra/security/SecurityConfigurations.java
@@ -24,6 +24,7 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return  httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configure(httpSecurity))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
