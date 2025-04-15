9bfde9aa6d385d56e0d274816d1cff6ce8450583
nelsonfjunior <ferreiranelson123@hotmail.com>
2025-04-14 21:41:10 -0300

    retornar o id do usuario no login

--- src/main/java/com/example/auth/controllers/AuthenticationController.java
+++ src/main/java/com/example/auth/controllers/AuthenticationController.java
@@ -34,7 +34,7 @@ public class AuthenticationController {
 
         var token = tokenService.generateToken((User) auth.getPrincipal());
 
-        return ResponseEntity.ok(new LoginResponseDTO(token));
+        return ResponseEntity.ok(new LoginResponseDTO(token, ((User) auth.getPrincipal()).getId()));
     }
 
     @PostMapping("/register")
