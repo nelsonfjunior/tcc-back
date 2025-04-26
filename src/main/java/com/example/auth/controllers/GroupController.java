42f9e48c72682c9aca6527e69d60c5352798765d
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-04-26 12:22:18 -0300

    feat: sobrescrita do método salvar para receber imagem

--- src/main/java/com/example/auth/controllers/GroupController.java
+++ src/main/java/com/example/auth/controllers/GroupController.java
@@ -1,5 +1,10 @@
 package com.example.auth.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,13 +12,21 @@ import com.example.auth.domain.group.Group;
 import com.example.auth.domain.group.GroupRequestDTO;
 import com.example.auth.domain.group.GroupResponseDTO;
 import com.example.auth.services.GenericServiceTypes;
+import com.example.auth.services.GroupService;
 
 @RestController
 @RequestMapping("/api/group")
 public class GroupController extends GenericController<Group, GroupResponseDTO, GroupRequestDTO>{
+    @Autowired
+    private GroupService groupService;
 
     protected GroupController(GenericServiceTypes<GroupResponseDTO, GroupRequestDTO> genericService) {
         super(genericService);
     }
 
+    @Override
+    @PostMapping
+    public ResponseEntity<GroupResponseDTO> save(@ModelAttribute GroupRequestDTO req) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(groupService.save(req));
+    }
 }
