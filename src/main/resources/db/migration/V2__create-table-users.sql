509aec9a42113628972c45af39284bd121175ec7
ViniciusFazolo <viniciusfazoloromanha@gmail.com>
2025-05-19 21:43:45 -0300

    remove flyway e corrige erro de mapstruct

--- src/main/resources/db/migration/V2__create-table-users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id TEXT PRIMARY KEY UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    login TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL,
-    role TEXT NOT NULL,
-    image BYTEA
-);
